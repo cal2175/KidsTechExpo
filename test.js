89465b919d7c09928ab2d0b804d1b6a114fd77b7
cal2175 <95599184+cal2175@users.noreply.github.com>
2023-01-18 12:04:40 +0900

    update

---
+++ test.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded',function(){
+    const videoElement = this.document.querySelector("video");
+
+    videoElement.muted = true;
+    videoElement.autoplay = true;
+})
